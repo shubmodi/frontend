5ba8a986bc1885e27741f147b363f95a2d159160
shubmodi <shubhammodi4702@gmail.com>
2024-08-06 20:27:02 +0530

    form validation

--- src/app/sign-up/page.jsx
+++ src/app/sign-up/page.jsx
@@ -1,4 +1,6 @@
 'use client'
+
+import { IconCircle, IconCircleCheck, IconLoader } from '@tabler/icons-react';
 import { useFormik } from 'formik'
 import React from 'react'
 import * as Yup from 'yup';
@@ -8,7 +10,14 @@ const SignupSchema = Yup.object().shape({
     .min(2, 'Too Short!')
     .max(50, 'Too Long!')
     .required('Required'),
-    email: Yup.string().email('Invalid email').required('Required'),
+    email: Yup.string().email('Invalid email').required('Email is Required'),
+    password : Yup.string().required('password is required')
+    .matches(/[a-z]/,'lowercase required')
+    .matches(/[A-Z]/,'uppercase required')
+    .matches(/[0-9]/,'number required')
+    .matches(/\W/,'special character is required'),
+    confirmPassword:Yup.string().required('confirm password is required')
+     .oneOf([Yup.ref('password'),null],'password must match')
  
 });
 
@@ -21,7 +30,13 @@ const page = () => {
       password : '',
       confirmPassword : '',
     },
-    onSubmit: (values) => {
+    onSubmit: (values , {resetForm,setSubmitting}) => {
+
+      setTimeout(() => {
+        console.log(values);
+        setSubmitting(false)
+      }, 3000);
+
       console.log(values);
       
     },
@@ -39,33 +54,36 @@ const page = () => {
        <label htmlFor="name">FullName</label>
        <span className='text-sm ml-4 text-red-500 '>{signupForm.touched.name && signupForm.errors.name}</span>
        <input id='name' onChange={signupForm.handleChange} value={signupForm.values.name} type="text" 
-       className={'border rounded w-full px-3 py-2 mb-4'+
+       className={'border rounded w-full px-3 py-2 mb-4 '+
         ((signupForm.touched.name && signupForm.errors.name)? 'border-red-500':'')
        }
        />  
        <label htmlFor="email">Email</label>
        <span className='text-sm ml-4 text-red-500 '>{signupForm.touched.email && signupForm.errors.email}</span>
        <input id='email' onChange={signupForm.handleChange} value={signupForm.values.email} type="email" 
-       className='border rounded w-full px-3 py-2 mb-4'
+       className={'border rounded w-full px-3 py-2 mb-4 '+
+       ((signupForm.touched.email && signupForm.errors.email)? 'border-red-500':'')
+       }
        />  
        <label htmlFor="password">Password</label>
        <span className='text-sm ml-4 text-red-500 '>{signupForm.touched.password && signupForm.errors.password}</span>
        <input id='password' onChange={signupForm.handleChange} value={signupForm.values.password} type="password" 
-       className={'border rounded w-full px-3 py-2 mb-4'+
+       className={'border rounded w-full px-3 py-2 mb-4 '+
         ((signupForm.touched.password && signupForm.errors.password)? 'border-red-500':'')
        }
        />  
        <label htmlFor="confirmpassword">Confirm Password</label>
        <span className='text-sm ml-4 text-red-500 '>{signupForm.touched.confirmPassword && signupForm.errors.confirmPassword}</span>
-       <input id='ConfirmPassword' onChange={signupForm.handleChange} value={signupForm.values.confirmPassword} type="password" 
-       className={'border rounded w-full px-3 py-2 mb-4'+
+       <input id='confirmPassword' onChange={signupForm.handleChange} value={signupForm.values.confirmPassword} type="password" 
+       className={'border rounded w-full px-3 py-2 mb-4 '+
         ((signupForm.touched.confirmPassword && signupForm.errors.confirmPassword)? 'border-red-500':'')
        }
        /> 
 
-       <button type='submit'
-       className='bg-blue-500 text-white px-3 py-2 mt-8 rounded w-full'>
-        submit
+       <button type='submit' disabled={signupForm.isSubmitting}
+       className='bg-blue-500 flex justify-center items-center text-white px-3 py-2 mt-8 rounded w-full disabled:opacity-50'>
+        {signupForm.isSubmitting?<IconLoader className='animate-spin' size={20} />:<IconCircleCheck size={20}/>}
+        <span>{signupForm.isSubmitting?'please wait':'submit'}</span>
         </button> 
        
        </form>
